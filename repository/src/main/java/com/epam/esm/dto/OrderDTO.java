794f354edd8beaa94227b4939d2b7bf8bfcb4ecc
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2024-01-16 15:40:14 +0500

    add fixed validations

--- repository/src/main/java/com/epam/esm/dto/OrderDTO.java
+++ repository/src/main/java/com/epam/esm/dto/OrderDTO.java
@@ -1,6 +1,7 @@
 package com.epam.esm.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 import java.io.Serializable;
 
@@ -8,7 +9,11 @@ import java.io.Serializable;
  * Data Transfer Object (DTO) representing an order.
  */
 public record OrderDTO(
-        @NotBlank Long userId,
-        @NotBlank Long giftCertificateId
+        @NotBlank
+        @Positive
+        Long userId,
+        @NotBlank
+        @Positive
+        Long giftCertificateId
 ) implements Serializable {
 }
--- repository/src/main/java/com/epam/esm/dto/UserRegisterDTO.java
+++ repository/src/main/java/com/epam/esm/dto/UserRegisterDTO.java
@@ -1,6 +1,8 @@
 package com.epam.esm.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
 
@@ -8,9 +10,15 @@ import java.io.Serializable;
  * Data Transfer Object (DTO) representing user registration information.
  */
 public record UserRegisterDTO(
-        @NotBlank String email,
-        @NotBlank String username,
+        @NotBlank(message = "Email must not be blank")
+        @Email(message = "Invalid email format")
+        String email,
 
-        @NotBlank String password
+        @NotBlank(message = "Username must not be blank")
+        String username,
+
+        @NotBlank(message = "Password must not be blank")
+        @Size(min = 8, message = "Password must be at least 8 characters long")
+        String password
 ) implements Serializable {
 }
