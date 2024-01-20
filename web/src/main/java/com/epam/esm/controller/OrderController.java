17b4bf120553e9f8f8e27635934d4012807e5400
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2024-01-20 18:38:11 +0500

    add refactored code

--- web/src/main/java/com/epam/esm/controller/OrderController.java
+++ web/src/main/java/com/epam/esm/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.epam.esm.controller;
 
+import com.epam.esm.config.security.CustomeUserDetails;
 import com.epam.esm.dto.OrderDTO;
 import com.epam.esm.entity.Order;
 import com.epam.esm.exception.NotFoundException;
@@ -7,14 +8,21 @@ import com.epam.esm.model.OrderModel;
 import com.epam.esm.model.assembler.OrderModelAssembler;
 import com.epam.esm.service.OrderService;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api/orders", produces = "application/json", consumes = "application/json")
 public class OrderController {
+
     private final OrderService orderService;
     private final OrderModelAssembler orderModelAssembler;
 
@@ -35,23 +43,63 @@ public class OrderController {
     public CollectionModel<OrderModel> getOrdersWithPage(@RequestParam(required = false, defaultValue = "1", name = "page") int page,
                                                          @RequestParam(required = false, defaultValue = "10", name = "size") int size) {
         List<Order> orders = orderService.findAllWithPage(page, size);
-
         return orderModelAssembler.toCollectionModel(orders, page, size);
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
-    public OrderModel createOrder(@RequestBody OrderDTO dto) {
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    public OrderModel createOrder(@RequestBody OrderDTO dto, Authentication authentication) {
+        checkUserPermissionForOrderCreation(dto, authentication);
         Order createdOrder = orderService.create(dto.userId(), dto.giftCertificateId());
         return orderModelAssembler.toModel(createdOrder);
     }
 
-    @GetMapping(value = "/{userId}/user-orders")
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @GetMapping(value = "/{userId}/orders")
+    @PreAuthorize("isAuthenticated()")
     public CollectionModel<OrderModel> getOrdersByUserIdWithPage(@PathVariable("userId") Long userId,
                                                                  @RequestParam(required = false, defaultValue = "1", name = "page") int page,
-                                                                 @RequestParam(required = false, defaultValue = "10", name = "size") int size) {
+                                                                 @RequestParam(required = false, defaultValue = "10", name = "size") int size,
+                                                                 Authentication authentication) {
+        checkUserPermissionForOrderView(userId, authentication);
         List<Order> userOrders = orderService.findOrdersInfoByUserIdWithPage(userId, page, size);
         return orderModelAssembler.toCollectionModel(userOrders, page, size);
+    }
+
+    @GetMapping(value = "/admin/{userId}/orders")
+    @PreAuthorize("hasRole('ADMIN')")
+    public CollectionModel<OrderModel> getOrdersByUserIdWithPageAdmin(@PathVariable("userId") Long userId,
+                                                                      @RequestParam(required = false, defaultValue = "1", name = "page") int page,
+                                                                      @RequestParam(required = false, defaultValue = "10", name = "size") int size) {
+        List<Order> userOrders = orderService.findOrdersInfoByUserIdWithPage(userId, page, size);
+        return orderModelAssembler.toCollectionModel(userOrders, page, size);
+    }
+
+    private void checkUserPermissionForOrderCreation(OrderDTO dto, Authentication authentication) {
+        Set<String> roles = getRolesFromAuthentication(authentication);
+        if (roles.contains("ROLE_ADMIN")) {
+            return;
+        }
+        checkUserPermissionForOrderView(dto.userId(), authentication);
+    }
+
+    private void checkUserPermissionForOrderView(Long userId, Authentication authentication) {
+        Long authenticatedUserId = getUserIdFromAuthentication(authentication);
+        if (!Objects.equals(userId, authenticatedUserId)) {
+            throw new AccessDeniedException("You do not have permission to view orders for another user.");
+        }
+    }
+
+    private Long getUserIdFromAuthentication(Authentication authentication) {
+        if (authentication.getPrincipal() instanceof CustomeUserDetails userDetails) {
+            return userDetails.getUser().getId();
+        } else {
+            throw new AccessDeniedException("User information not available in the authentication.");
+        }
+    }
+
+    private Set<String> getRolesFromAuthentication(Authentication authentication) {
+        return authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet());
     }
 }
